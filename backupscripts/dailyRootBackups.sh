dc363b62bfbad2274779cc35ce738729947a0f06
BuildTools <unconfigured@null.spigotmc.org>
2020-03-29 23:23:38 +0200

    fixed line ending and few bugs

--- backupscripts/dailyRootBackups.sh
+++ backupscripts/dailyRootBackups.sh
@@ -1,11 +1,10 @@
-targetdir=home/minecraft/backups/system
+#!/bin/bash
+targetdir=/home/minecraft/backups/system
 
 # get installed packages
 dpkg --get-selections > "$targetdir/installed_packages"
 
 # minecracy mysql databases
 for DB in $(mysql -e 'show databases' -s --skip-column-names); do
-    if [[ $DB == my_* ]]; then
-        mysqldump $DB | gzip -c > "$targetdir/$DB.sql.gz";
-    fi
+    mysqldump $DB | gzip -c > "$targetdir/$DB.sql.gz";
 done
