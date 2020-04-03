cf4b1689c40ccf0412614519b38a33447b3d346b
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 19:38:05 +0200

    added excluded dirs

--- backupscripts/dailyMyServerBackup.sh
+++ backupscripts/dailyMyServerBackup.sh
@@ -23,12 +23,21 @@ done
 
 # Sync backup data
 echo "Syncing backup files ..."
-borg create -v --stats                  \
-    $REPOSITORY::'{now:%Y-%m-%d_%H:%M}' \
-    /root                               \
-    /etc                                \
-    /var/www                            \
-    /var/mail                           \
-    /home
+borg create -v --stats                                  \
+    $REPOSITORY::'{now:%Y-%m-%d_%H:%M}'                 \
+    /root                                               \
+    /etc                                                \
+    /var/www                                            \
+    /var/mail                                           \
+    /home                                               \
+    --exclude /home/minecraft/config                    \
+    --exclude /home/minecraft/mods                      \
+    --exclude /home/minecraft/plugins                   \
+    --exclude /home/minecraft/world5                    \
+    --exclude /home/minecraft/world5_nether             \
+    --exclude /home/minecraft/world5_the_end            \
+    --exclude '/home/minecraft/world6*'                 \
+    --exclude '/home/minecraft/maps/*/.git/objects/*'   \
+    --exclude '/home/minecraft/maps/BlockMap/*/build'
 
 echo "Finished backup on $(date)"
